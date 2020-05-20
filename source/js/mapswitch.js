f986ddf59191d48abf82f4916230901625d03f45
Foma <fom-bagiyan@yandex.ru>
2020-05-20 14:21:22 +0300

    Finished with styles

---
+++ source/js/mapswitch.js
@@ -0,0 +1,5 @@
+  var mapInteractive = document.querySelector('.location__map-interactive');
+  var mapContainer = document.querySelector('.location__map-container');
+
+  mapInteractive.classList.remove('location__map-interactive--no-js');
+  mapContainer.classList.remove('location__map-container--no-js');
